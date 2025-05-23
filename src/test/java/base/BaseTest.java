f0f9f4414a27492a4d5d3778c3e08637fa27474c
Yurii Morozov <morozov.yurik@gmail.com>
2025-05-23 11:50:34 +0200

    Update infrastructure

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,8 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
-import utils.DriverFactory;
-import utils.PageFactory;
 
 @Listeners({io.qameta.allure.testng.AllureTestNg.class})
 public class BaseTest {
--- src/test/java/base/DriverFactory.java
+++ src/test/java/base/DriverFactory.java
@@ -1,4 +1,4 @@
-package utils;
+package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
